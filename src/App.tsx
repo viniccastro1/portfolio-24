b9304ecdea4976716867af58ebb143c830e2ee18
Castro <viniccastro@outlook.com>
2024-06-12 11:30:07 -0300

    inital libs installed

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  function myfunc() {
+    return 2;
+  }
+
+  function myfunc2() {
+    return 2;
+  }
+
+  return (
+    <>
+      <div className="fs text-gray-600">asdasd</div>
+    </>
+  );
+}
+
+export default App;
